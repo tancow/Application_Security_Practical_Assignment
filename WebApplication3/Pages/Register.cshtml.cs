dece9a28660a8aa0fafd48497a4a655f2f4d7bbe
tancow <kahhowcow@gmail.com>
2023-02-06 17:15:14 +0800

    Add project files.

---
+++ WebApplication3/Pages/Register.cshtml.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication3.ViewModels;
+
+namespace WebApplication3.Pages
+{
+    public class RegisterModel : PageModel
+    {
+
+        private UserManager<IdentityUser> userManager { get; }
+        private SignInManager<IdentityUser> signInManager { get; }
+
+        private IWebHostEnvironment _environment;
+
+        [BindProperty]
+        public Register RModel { get; set; }
+        public IFormFile? Upload { get; set; }
+
+        public RegisterModel(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IWebHostEnvironment environment)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+            _environment = environment;
+        }
+
+
+
+        public void OnGet()
+        {
+        }
+
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser()
+                {
+                    UserName = RModel.Email,
+                    Email = RModel.Email
+                };
+                var result = await userManager.CreateAsync(user, RModel.Password);
+ 
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(user, false);
+                    return RedirectToPage("Index");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                //if (Upload != null)
+                //{
+                //    if (Upload.Length > 2 * 1024 * 1024)
+                //    {
+                //        ModelState.AddModelError("Upload",
+                //        "File size cannot exceed 2MB.");
+                //        return Page();
+                //    }
+                //    var uploadsFolder = "uploads";
+                //    var imageFile = Guid.NewGuid() + Path.GetExtension(
+                //    Upload.FileName);
+                //    var imagePath = Path.Combine(_environment.ContentRootPath,
+                //    "wwwroot", uploadsFolder, imageFile);
+                //    using var fileStream = new FileStream(imagePath,
+                //    FileMode.Create);
+                //    await Upload.CopyToAsync(fileStream);
+                //    Register.ImageURL = string.Format("/{0}/{1}", uploadsFolder,
+                //    imageFile);
+                //}
+
+            }
+            return Page();
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ WebApplication3/ViewModels/Register.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace WebApplication3.ViewModels
+{
+    public class Register
+    {
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string FullName { get; set; }
+
+
+        [Required]
+        [DataType(DataType.CreditCard)]
+        public string CreditCard { get; set; }
+
+
+        [Required, MaxLength(1)]
+        public string Gender { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string DeliveryAddress { get; set; }
+
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = "Password and confirmation password does not match")]
+        public string ConfirmPassword { get; set; }
+
+
+        [MaxLength(50)]
+        public string? ImageURL { get; set; }
+
+
+        [DataType(DataType.MultilineText)]
+        public string? AboutMe { get; set; }
+
+    }
+}
